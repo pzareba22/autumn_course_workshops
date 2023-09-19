91017169c8ba02ad3b25741ad0d193517caed335
Paweł Zaręba <59089750+pzareba22@users.noreply.github.com>
2023-09-19 14:40:00 +0200

    Update seedSalted.js

--- seedSalted.js
+++ seedSalted.js
@@ -14,7 +14,6 @@ db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS Messages(id INTEGER PRIMARY KEY, data TEXT)");
   db.run("CREATE TABLE IF NOT EXISTS Messages2(id INTEGER PRIMARY KEY, data TEXT)");
   db.run("CREATE TABLE IF NOT EXISTS Secret(id INTEGER PRIMARY KEY, data TEXT)");
-  db.run("INSERT INTO Users (username, password) VALUES ('admin', 'hehe123')");
   db.run("INSERT INTO Messages(data) VALUES ('Hello World')");
   db.run("INSERT INTO Messages(data) VALUES ('Lorem Ipsum')");
   db.run("INSERT INTO Messages2(data) VALUES ('Another Message')");
